c15e53e8eb6603790f012df0f3dc6ed76e33fa97
MirceaObreja <47657440+MirceaObreja@users.noreply.github.com>
2019-10-21 16:08:10 +0200

    neuer ordner für rechnungen

---
+++ demos/rechnungen/formeln.py
@@ -0,0 +1,10 @@
+def kalorien_berechnen(alter):
+    neuer_preis = alter - (alter * 0.1)
+    return neuer_preis
+
+
+
+
+def grosser_rabatt_berechnen(preis):
+    neuer_preis = preis - (preis * 0.25)
+    return neuer_preis
