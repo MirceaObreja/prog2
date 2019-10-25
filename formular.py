c38715af1a2a0c3ae4a5f07099975b546543de9f
MirceaObreja <47657440+MirceaObreja@users.noreply.github.com>
2019-10-25 13:28:29 +0200

    idex bearbeitet

--- formular.py
+++ formular.py
@@ -4,19 +4,14 @@ from flask import request
 
 app = Flask("Demo")
 
-
-
-@app.route("/kalorienrechner/", methods=['GET', 'POST'])
+@app.route("/hello/", methods=['GET', 'POST'])
 def hallo():
 	if request.method == 'POST':
 		ziel_person = request.form['vorname']
 		rueckgabe_string = "Hello " + ziel_person + "!"
 		return rueckgabe_string
 
-	return render_template("index.html", dinger= 2)
+	return render_template("index.html")
 
-
-
-
-if __name__ == "__main__":
+if __name__ == "__main__"
 	app.run(debug=True, port=5000)
