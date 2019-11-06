8edbadcb94876480e3633064e59cecb2fec097c9
MirceaObreja <47657440+MirceaObreja@users.noreply.github.com>
2019-11-06 15:41:20 +0100

    anpassungen

--- demos2/main.py
+++ demos2/main.py
@@ -3,39 +3,53 @@ from flask import render_template
 from flask import redirect
 from flask import request
 from flask import url_for
+import os
 
 from libs import suppliste
+
+os.system("cls")
 
 app = Flask("Telefonbuch")
 
 @app.route("/index")
 def index():
-    telbuchdaten = suppliste.telefonbuch_lesen()
-    return render_template("suppliste.html", telbuch=telbuchdaten)
+    prod_daten = suppliste.file_lesen('prod.txt')
+    print(prod_daten)
+    return render_template("suppliste.html", prod=prod_daten)
 
 @app.route("/" , methods=['GET', 'POST'])
 @app.route("/kal", methods=['GET', 'POST'])
 def kal():
     if (request.method == 'POST'):
-        suppliste.eintrag_speichern_von_formular(request.form)
+        suppliste.eintrag_speichern_von_kalo(request.form)
         return redirect("/")
 
     return render_template("kalo.html")
 
-@app.route("/search/<name>")
 @app.route("/search", methods=['GET', 'POST'])
 def search(name=None):
     if (request.method == 'POST'):
         produkt_eintrag = suppliste.produkt_suchen(request.form)
         print(produkt_eintrag)
-        return render_template("suppliste.html", telbuch=produkt_eintrag)
+        return render_template("suppliste.html", prod=produkt_eintrag)
 
     return render_template("search.html")
+
+
+@app.route("/search_per", methods=['GET', 'POST'])
+def search_per(name=None):
+    if (request.method == 'POST'):
+        person_eintrag = suppliste.person_suchen(request.form)
+        print(person_eintrag)
+        return render_template("personenliste.html", prod=person_eintrag)
+
+    return render_template("personenliste.html")
+
 
 @app.route("/add", methods=['GET', 'POST']) 
 def add():
     if (request.method == 'POST'):
-        suppliste.eintrag_speichern_von_formular(request.form)
+        suppliste.eintrag_speichern_von_prod(request.form)
         return redirect("/")
 
     return render_template("add.html")
