02f2a478e8f4bea89b978544fbb64b2a08c65349
MirceaObreja <47657440+MirceaObreja@users.noreply.github.com>
2019-11-16 09:30:07 +0100

    anpassung an der kalorien formel

--- demos2/rechnungen/formeln.py
+++ demos2/rechnungen/formeln.py
@@ -4,14 +4,13 @@ def kalorien_berechnen(alter):
 
 
 def grundumsatz(request):
-	geschlecht = request.form
     if geschlecht == "m":
-		grundumsatz = (10 * int(gewicht)) + (6.25 * int(groesse)) - (5 * int(alter)) + 5 
-		kalorien_bedarf = grundumsatz * faktor_m
-	else:
-		grundumsatz = (10 * int(gewicht)) + (6.25 * int(groesse)) - (5 * int(alter)) - 161
-		kalorien_bedarf = grundumsatz * faktor_w
-	return kalorien_bedarf
+    	grundumsatz = (10 * int(gewicht)) + (6.25 * int(groesse)) - (5 * int(alter)) + 5 
+    	kalorien_bedarf = grundumsatz * faktor_m
+    else:
+    	grundumsatz = (10 * int(gewicht)) + (6.25 * int(groesse)) - (5 * int(alter)) - 161
+    	kalorien_bedarf = grundumsatz * faktor_w
+    	return kalorien_bedarf
 
 
 def kalorienbedarf():
@@ -21,10 +20,17 @@ def kalorienbedarf():
 		kalorien_bedarf = grundumsatz * faktor_m
 	return kalorien_bedarf
 
-def basic(request):
-	
-	if geschlecht_weiblich == "weiblich":
-		kalorien_bedarf = alter * 3
+def basic(geschlecht, alter, groesse, gewicht, aktivitaet, ziel, training):
+	alter = int(alter)
+	groesse = int(groesse)
+	gewicht = int(gewicht)
+	aktivitaet = float(aktivitaet)
+	ziel = float(ziel)
+	training = int(training)
+
+	if geschlecht == "w":
+		kalorien_bedarf = ((10 * gewicht) + (6.25 * groesse) - (5 * alter) - 161) * aktivitaet
 	else:
-		kalorien_bedarf = alter * 5
+		kalorien_bedarf = ((10 * gewicht) + (6.25 * groesse) - (5 * alter) + 5) * aktivitaet 
+	
 	return kalorien_bedarf
